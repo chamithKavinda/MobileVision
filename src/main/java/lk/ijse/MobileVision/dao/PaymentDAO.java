674ff495538d26a994ef7ec0c4b01eb119a059e7
chamithKavinda <chamith13kavinda@gmail.com>
2024-01-06 11:28:28 +0530

    complete PaymentDAOImpl

---
+++ src/main/java/lk/ijse/MobileVision/dao/PaymentDAO.java
@@ -0,0 +1,13 @@
+package lk.ijse.MobileVision.dao;
+
+import lk.ijse.MobileVision.dto.PaymentDto;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface PaymentDAO {
+    boolean deletePayment(String p_id) throws SQLException;
+    boolean savePayment(PaymentDto dto) throws SQLException;
+    boolean updatePayment(PaymentDto dto) throws SQLException;
+    List<PaymentDto> getAllPayment() throws SQLException;
+}
